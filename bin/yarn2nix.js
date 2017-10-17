a9cf17ceb1ec11031cb7b4a2bb96f51213a08088
zimbatm <zimbatm@zimbatm.com>
2017-10-17 14:08:01 +0100

    introduce a yarn.lock patch phase
    For resources loaded directly from github, add the missing SHA1 when
    invoking yarn2nix.
    
    yarn doesn't touch entries unless they are modified directly so
    it should only be needed when bumping the dependencies directly.
    
    Ideally this would be supported by upstream.

--- bin/yarn2nix.js
+++ bin/yarn2nix.js
@@ -1,15 +1,34 @@
 #!/usr/bin/env node
 "use strict";
 
+const crypto = require('crypto');
 const fs = require("fs");
-const lockfile = require('@yarnpkg/lockfile')
+const https = require("https");
 const path = require("path");
+const util = require("util");
+
+const lockfile = require("@yarnpkg/lockfile")
+const docopt = require("docopt").docopt;
+
+////////////////////////////////////////////////////////////////////////////////
+
+const USAGE = `
+Usage: yarn2nix [options]
+
+Options:
+  -h --help        Shows this help.
+  --no-nix         Hide the nix output
+  --no-patch       Don't patch the lockfile if hashes are missing
+  --lockfile=FILE  Specify path to the lockfile [default: ./yarn.lock].
+`
 
 const HEAD = `
 {fetchurl, linkFarm}: rec {
   offline_cache = linkFarm "offline" packages;
   packages = [
 `.trim();
+
+////////////////////////////////////////////////////////////////////////////////
 
 function generateNix(lockedDependencies) {
   let found = {};
@@ -45,15 +64,81 @@ function generateNix(lockedDependencies) {
   console.log("}")
 }
 
+
+function getSha1(url) {
+  return new Promise((resolve, reject) => {
+    https.get(url, (res) => {
+      const { statusCode } = res;
+      const hash = crypto.createHash('sha1');
+      if (statusCode !== 200) {
+        const err = new Error('Request Failed.\n' +
+                          `Status Code: ${statusCode}`);
+        // consume response data to free up memory
+        res.resume();
+        reject(err);
+      }
+
+      res.on('data', (chunk) => { hash.update(chunk); });
+      res.on('end', () => { resolve(hash.digest('hex')) });
+      res.on('error', reject);
+    });
+  });
+};
+
+function updateResolvedSha1(pkg) {
+  // local dependency
+  if (!pkg.resolved) { return Promise.resolve(); }
+  let [url, sha1] = pkg.resolved.split("#", 2)
+  if (!sha1) {
+    return new Promise((resolve, reject) => {
+      getSha1(url).then(sha1 => {
+        pkg.resolved = `${url}#${sha1}`;
+        resolve();
+      }).catch(reject);
+    });
+  } else {
+    // nothing to do
+    return Promise.resolve();
+  };
+}
+
+function values(obj) {
+  var entries = [];
+  for (let key in obj) {
+    entries.push(obj[key]);
+  }
+  return entries;
+}
+
+////////////////////////////////////////////////////////////////////////////////
 // Main
+////////////////////////////////////////////////////////////////////////////////
 
-const yarnLock = process.argv[2] || "yarn.lock";
+var options = docopt(USAGE);
 
-let file = fs.readFileSync(yarnLock, 'utf8')
-let json = lockfile.parse(file)
-
+let data = fs.readFileSync(options['--lockfile'], 'utf8')
+let json = lockfile.parse(data)
 if (json.type != "success") {
   throw new Error("yarn.lock parse error")
 }
 
-generateNix(json.object);
+// Check fore missing hashes in the yarn.lock and patch if necessary
+var pkgs = values(json.object);
+Promise.all(pkgs.map(updateResolvedSha1)).then(() => {
+  let newData = lockfile.stringify(json.object);
+
+  if (newData != data) {
+    console.error("found changes in the lockfile", options["--lockfile"]);
+
+    if (options["--no-patch"]) {
+      console.error("...aborting");
+      process.exit(1);
+    }
+
+    fs.writeFileSync(options['--lockfile'], newData);
+  }
+
+  if (!options['--no-nix']) {
+    generateNix(json.object);
+  }
+})
