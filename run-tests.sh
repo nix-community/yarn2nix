ffa06ac84a5158cf97609f6c5249cdf0dc8b36c1
zimbatm <zimbatm@zimbatm.com>
2018-11-05 16:18:23 +0100

    fixup! tests: add import-from-derivation test

--- run-tests.sh
+++ run-tests.sh
@@ -5,15 +5,16 @@ set -euo pipefail
 
 nix_options=(
   --no-out-link
-  #--option allow-import-from-derivation false
-  #--option restrict-eval true
   --option sandbox true
   --show-trace
   -I "nixpkgs=channel:$NIXPKGS_CHANNEL"
 )
 
-nix-build tests "${nix_options[@]}"
+echo "--- Running tests with IFD enabled"
+nix-build tests "${nix_options[@]}" \
+  --option allow-import-from-derivation true
 
+echo "--- Running tests with IFD disabled"
 nix-build tests "${nix_options[@]}" \
   --option allow-import-from-derivation false \
   -A no-import-from-derivation
