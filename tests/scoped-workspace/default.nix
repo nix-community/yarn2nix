daf01eec7a9b52487fd473da6b7ac37372527f58
Sergei Khoma <srghma@gmail.com>
2019-01-10 20:23:44 +0200

    fix: scoped workspaces -> add not working test

---
+++ tests/scoped-workspace/default.nix
@@ -0,0 +1,22 @@
+{ yarn2nix }:
+
+yarn2nix.mkYarnWorkspace {
+  src = ./.;
+
+  packageOverrides = {
+    testcompany-one = {
+      publishBinsFor = [ "testcompany-one" "gulp" ];
+
+      doInstallCheck = true;
+      installCheckPhase = ''
+        ${import ../../nix/expectShFunctions.nix}
+
+        output=$($out/bin/testcompany-one)
+        expected_output="HELLO FROM @TESTCOMPANY/TWO!"
+        expectEqual "$output" "$expected_output"
+
+        $out/bin/gulp --help
+      '';
+    };
+  };
+}
